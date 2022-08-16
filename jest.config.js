84cb42e986ab4f3c8fc4b15970a35a4f8e5d934e
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-08-16 14:36:17 -0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
