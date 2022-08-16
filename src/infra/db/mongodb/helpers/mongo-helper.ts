3e84cff44f27151f8957f5a7eb4c5b486f804375
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-08-16 16:33:14 -0300

    feat: ensure mongodb reconnect is client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,25 +1,31 @@
-import { MongoClient, Collection } from 'mongodb'
+import { MongoClient, Collection } from "mongodb";
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
 
-  async connect (uri: string): Promise<void> {
+  async connect(uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+      useUnifiedTopology: true,
+    });
   },
 
-  async disconnect (): Promise<void> {
-    await this.client.close()
+  async disconnect(): Promise<void> {
+    await this.client.close();
+    this.client = null;
   },
 
-  getCollection (name: string): Collection {
-    return this.client.db().collection(name)
+  async getCollection(name: string): Promise<Collection> {
+    if(!this.client?.isConnected()){
+      await this.connect(this.uri);
+    }
+    return this.client.db().collection(name);
   },
 
   map: (collection: any): any => {
-    const { _id, ...collectionWithoutId } = collection
-    return Object.assign({}, collectionWithoutId, { id: _id })
-  }
-}
+    const { _id, ...collectionWithoutId } = collection;
+    return Object.assign({}, collectionWithoutId, { id: _id });
+  },
+};
