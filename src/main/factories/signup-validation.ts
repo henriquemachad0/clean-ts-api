b15c80295d4543b3b7c6d0c696b2c083e64e568c
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-08-18 16:22:58 -0300

    refactor: move required field validation to the ValidationComposite

---
+++ src/main/factories/signup-validation.ts
@@ -0,0 +1,12 @@
+import { RequiredFielValidation } from "../../presentation/helpers/validators/required-field-validation";
+import { Validation } from "../../presentation/helpers/validators/validation";
+import { ValidationComposite } from "../../presentation/helpers/validators/validation-composite";
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = [];
+  for(const field of ['name', 'email', 'password', 'passwordConfirmation']){
+    validations.push(new RequiredFielValidation(field));
+  }
+  
+  return new ValidationComposite(validations)
+};
