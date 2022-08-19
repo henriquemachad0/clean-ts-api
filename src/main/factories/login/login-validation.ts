a60a112c9624c356e4c05a3422d9df79011210d2
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-08-19 14:21:56 -0300

    refactor: group validations imports in a single file

--- src/main/factories/login/login-validation.ts
+++ src/main/factories/login/login-validation.ts
@@ -1,14 +1,16 @@
-import { EmailValidation } from "../../../presentation/helpers/validators/email-validation";
-import { RequiredFielValidation } from "../../../presentation/helpers/validators/required-field-validation";
+import {
+  ValidationComposite,
+  RequiredFielValidation,
+  EmailValidation,
+} from "../../../presentation/helpers/validators";
 import { Validation } from "../../../presentation/protocols/validation";
-import { ValidationComposite } from "../../../presentation/helpers/validators/validation-composite";
 import { EmailValidatorAdapter } from "../../../utils/email-validator-adapter";
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = [];
-  for(const field of ['email', 'password']){
+  for (const field of ["email", "password"]) {
     validations.push(new RequiredFielValidation(field));
   }
-  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
-  return new ValidationComposite(validations)
+  validations.push(new EmailValidation("email", new EmailValidatorAdapter()));
+  return new ValidationComposite(validations);
 };
