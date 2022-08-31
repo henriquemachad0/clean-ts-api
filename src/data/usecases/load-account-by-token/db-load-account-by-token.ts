96ba938f19066085b4ce71573c3a1ff614db2fa6
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-08-31 18:00:10 -0300

    feat: ensure DbLoadAccountByToken returns an account on soccess

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -7,13 +7,19 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor(
     private readonly decrypter: Decrypter,
     private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
-    ) {}
+  ) {}
 
   async load(accessToken: string, role?: string): Promise<AccountModel> {
-    const token = await this.decrypter.decrypt(accessToken)
-    if(token){
-        await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+    const token = await this.decrypter.decrypt(accessToken);
+    if (token) {
+      const account = await this.loadAccountByTokenRepository.loadByToken(
+        accessToken,
+        role
+      );
+      if (account) {
+        return account;
+      }
     }
-    return null
+    return null;
   }
 }
