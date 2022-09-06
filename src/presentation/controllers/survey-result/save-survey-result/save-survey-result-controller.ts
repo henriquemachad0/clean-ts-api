9f220d0592b37a76de920d68c5bc76ec0a84273a
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-09-06 15:11:18 -0300

    feat: ensure SaveSurverResultController returns 500 if SaveSurveyResult throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,6 +1,7 @@
 import { InvalidParamError } from "@/presentation/errors";
 import {
   forbidden,
+  ok,
   serverError,
 } from "@/presentation/helpers/http/http-helper";
 import { throws } from "assert";
@@ -32,13 +33,13 @@ export class SaveSurveyResultController implements Controller {
       } else {
         return forbidden(new InvalidParamError("surveyId"));
       }
-      await this.saveSurveyResult.save({
+      const surveyResult = await this.saveSurveyResult.save({
         accountId,
         surveyId,
         answer,
         date: new Date(),
       });
-      return null;
+      return ok(surveyResult);
     } catch (error) {
       return serverError(error);
     }
