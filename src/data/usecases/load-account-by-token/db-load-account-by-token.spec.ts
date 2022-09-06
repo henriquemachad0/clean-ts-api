a7f9d32db1320d1b3eb8cfe01123c82f5327aadd
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-09-06 09:17:57 -0300

    refactor: add data usecases protocol files to simplify imports

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -1,7 +1,9 @@
 import { DbLoadAccountByToken } from "./db-load-account-by-token";
-import { Decrypter } from "@/data/protocols/criptography/decrypter";
-import { LoadAccountByTokenRepository } from "@/data/protocols/db/account/load-account-by-token-repository";
-import { AccountModel } from "@/domain/models/account";
+import {
+  Decrypter,
+  LoadAccountByTokenRepository,
+  AccountModel,
+} from "./db-load-account-by-token-protocols";
 
 const makeFakeAccount = (): AccountModel => ({
   id: "valid_id",
@@ -34,7 +36,7 @@ type SutTypes = {
   sut: DbLoadAccountByToken;
   decrypterStub: Decrypter;
   loadAccountByTokenRepositoryStub: LoadAccountByTokenRepository;
-}
+};
 
 const makeSut = (): SutTypes => {
   const decrypterStub = makeDecrypter();
