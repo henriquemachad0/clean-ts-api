ee2cae8f7edac8f4ab478e5b083a6cfb29706808
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-09-12 17:04:22 -0300

    refactor: move LoadSurveyResult dependencies to a protocols file

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -0,0 +1,10 @@
+import { LoadSurveyResultRepository, SurveyResultModel, LoadSurveyResult } from './db-load-survey-result-protocols'
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+  constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+
+  async load (surveyId: string): Promise<SurveyResultModel> {
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    return surveyResult
+  }
+}
