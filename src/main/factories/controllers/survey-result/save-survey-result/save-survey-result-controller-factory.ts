42d3d442ac4a1c3bec4e301ee266d59767ad5cfe
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-09-13 15:31:17 -0300

    feat: ensure load survey route returns 403 if no accessToken is provided

--- src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
@@ -2,7 +2,7 @@ import { Controller } from "@/presentation/protocols";
 import { makeLogControllerDecorator } from "@/main/factories/decorators/log-controller-decorator-factory";
 import { SaveSurveyResultController } from "@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller";
 import { makeDbLoadSurveyById } from "@/main/factories/usecases/survey/load-survey-by-id/db-load-survey-by-id-factory";
-import { makeDbSaveSurveyResult } from "@/main/factories/usecases/survey/survey-result/save-survey-result/db-save-survey-result-factory";
+import { makeDbSaveSurveyResult } from "@/main/factories/usecases/survey-result/save-survey-result/db-save-survey-result-factory";
 
 export const makeSaveSurveyResultController = (): Controller => {
   const controller = new SaveSurveyResultController(makeDbLoadSurveyById(), makeDbSaveSurveyResult());
