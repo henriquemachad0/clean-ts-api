224514e36f8844150a987246c6991f88d55f52b8
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-09-13 16:58:39 -0300

    feat: add name to authentication response

--- src/presentation/test/mock-account.ts
+++ src/presentation/test/mock-account.ts
@@ -1,38 +1,50 @@
-import { AddAccount, AddAccountParams } from '@/domain/usecases/account/add-account'
-import { Authentication, AuthenticationParams } from '@/domain/usecases/account/authentication'
-import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
-import { AccountModel } from '@/domain/models/account'
-import { mockAccountModel } from '@/domain/test'
-import faker from 'faker'
+import {
+  AddAccount,
+  AddAccountParams,
+} from "@/domain/usecases/account/add-account";
+import {
+  Authentication,
+  AuthenticationParams,
+} from "@/domain/usecases/account/authentication";
+import { LoadAccountByToken } from "@/domain/usecases/account/load-account-by-token";
+import { AccountModel } from "@/domain/models/account";
+import { AuthenticationModel } from "@/domain/models/authentication";
+import { mockAccountModel } from "@/domain/test";
+import faker from "faker";
 
 export class AddAccountSpy implements AddAccount {
-  accountModel = mockAccountModel()
-  addAccountParams: AddAccountParams
+  accountModel = mockAccountModel();
+  addAccountParams: AddAccountParams;
 
-  async add (account: AddAccountParams): Promise<AccountModel> {
-    this.addAccountParams = account
-    return Promise.resolve(this.accountModel)
+  async add(account: AddAccountParams): Promise<AccountModel> {
+    this.addAccountParams = account;
+    return Promise.resolve(this.accountModel);
   }
 }
 
 export class AuthenticationSpy implements Authentication {
-  authenticationParams: AuthenticationParams
-  token = faker.random.uuid()
+  authenticationParams: AuthenticationParams;
+  authenticationModel = {
+    accessToken: faker.random.uuid(),
+    name: faker.name.findName(),
+  };
 
-  async auth (authenticationParams: AuthenticationParams): Promise<string> {
-    this.authenticationParams = authenticationParams
-    return Promise.resolve(this.token)
+  async auth(
+    authenticationParams: AuthenticationParams
+  ): Promise<AuthenticationModel> {
+    this.authenticationParams = authenticationParams;
+    return this.authenticationModel
   }
 }
 
 export class LoadAccountByTokenSpy implements LoadAccountByToken {
-  accountModel = mockAccountModel()
-  accessToken: string
-  role: string
+  accountModel = mockAccountModel();
+  accessToken: string;
+  role: string;
 
-  async load (accessToken: string, role?: string): Promise<AccountModel> {
-    this.accessToken = accessToken
-    this.role = role
-    return Promise.resolve(this.accountModel)
+  async load(accessToken: string, role?: string): Promise<AccountModel> {
+    this.accessToken = accessToken;
+    this.role = role;
+    return Promise.resolve(this.accountModel);
   }
 }
