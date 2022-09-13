2093f6ebf9773680f0254e22bdddc62171897940
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-09-13 15:07:35 -0300

    feat: ensure LoadSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,16 +1,29 @@
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './load-survey-result-controller-protocols'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
-import { InvalidParamError } from '@/presentation/errors'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  LoadSurveyById,
+} from "./load-survey-result-controller-protocols";
+import {
+  forbidden,
+  serverError,
+} from "@/presentation/helpers/http/http-helper";
+import { InvalidParamError } from "@/presentation/errors";
 
 export class LoadSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor(private readonly loadSurveyById: LoadSurveyById) {}
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyById.loadById(httpRequest.params.surveyId)
-    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
-    if (!survey) {
-      return forbidden(new InvalidParamError('surveyId'))
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const survey = await this.loadSurveyById.loadById(
+        httpRequest.params.surveyId
+      );
+      if (!survey) {
+        return forbidden(new InvalidParamError("surveyId"));
+      }
+      return null;
+    } catch (error) {
+      return serverError(error);
     }
-    return Promise.resolve(null)
   }
 }
