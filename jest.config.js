4bf1768afb721cb262ea97e4a45154a101f79f2f
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-09-14 11:36:54 -0300

    refactor: isolate tests in a special folder configs

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/tests'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**',
-    '!**/test/**'
+    '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-  moduleNameMapper:{
+  moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1'
   }
 }
