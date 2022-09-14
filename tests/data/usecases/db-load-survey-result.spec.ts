f16864911bfd983e206a952201d1de27d40882d6
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2022-09-14 18:26:05 -0300

    refactor: simplify Object.assign with new spread operator sintax

--- tests/data/usecases/db-load-survey-result.spec.ts
+++ tests/data/usecases/db-load-survey-result.spec.ts
@@ -1,16 +1,13 @@
 import { DbLoadSurveyResult } from '@/data/usecases'
-import { LoadSurveyResultRepositorySpy, LoadSurveyByIdRepositorySpy } from '@/tests/data/mocks'
-import { throwError } from '@/tests/domain/mocks'
-
+import { LoadSurveyResultRepositorySpy, LoadSurveyByIdRepositorySpy } from '@/../tests/data/mocks'
+import { throwError } from '@/../tests/domain/mocks'
 import MockDate from 'mockdate'
 import faker from 'faker'
-
 type SutTypes = {
   sut: DbLoadSurveyResult
   loadSurveyResultRepositorySpy: LoadSurveyResultRepositorySpy
   loadSurveyByIdRepositorySpy: LoadSurveyByIdRepositorySpy
 }
-
 const makeSut = (): SutTypes => {
   const loadSurveyResultRepositorySpy = new LoadSurveyResultRepositorySpy()
   const loadSurveyByIdRepositorySpy = new LoadSurveyByIdRepositorySpy()
@@ -21,55 +18,49 @@ const makeSut = (): SutTypes => {
     loadSurveyByIdRepositorySpy
   }
 }
-
 let surveyId: string
 let accountId: string
-
 describe('DbLoadSurveyResult UseCase', () => {
   beforeAll(() => {
     MockDate.set(new Date())
   })
-
   afterAll(() => {
     MockDate.reset()
   })
 
   beforeEach(() => {
-    surveyId = faker.datatype.uuid()
-    accountId = faker.datatype.uuid()
+    surveyId = faker.random.uuid()
+    accountId = faker.random.uuid()
   })
-
   test('Should call LoadSurveyResultRepository with correct values', async () => {
     const { sut, loadSurveyResultRepositorySpy } = makeSut()
     await sut.load(surveyId, accountId)
     expect(loadSurveyResultRepositorySpy.surveyId).toBe(surveyId)
     expect(loadSurveyResultRepositorySpy.accountId).toBe(accountId)
   })
-
   test('Should throw if LoadSurveyResultRepository throws', async () => {
     const { sut, loadSurveyResultRepositorySpy } = makeSut()
     jest.spyOn(loadSurveyResultRepositorySpy, 'loadBySurveyId').mockImplementationOnce(throwError)
     const promise = sut.load(surveyId, accountId)
     await expect(promise).rejects.toThrow()
   })
-
   test('Should call LoadSurveyByIdRepository if LoadSurveyResultRepository returns null', async () => {
     const { sut, loadSurveyResultRepositorySpy, loadSurveyByIdRepositorySpy } = makeSut()
-    loadSurveyResultRepositorySpy.result = null
+    loadSurveyResultRepositorySpy.surveyResultModel = null
     await sut.load(surveyId, accountId)
     expect(loadSurveyByIdRepositorySpy.id).toBe(surveyId)
   })
 
   test('Should return surveyResultModel with all answers with count 0 if LoadSurveyResultRepository returns null', async () => {
     const { sut, loadSurveyResultRepositorySpy, loadSurveyByIdRepositorySpy } = makeSut()
-    loadSurveyResultRepositorySpy.result = null
+    loadSurveyResultRepositorySpy.surveyResultModel = null
     const surveyResult = await sut.load(surveyId, accountId)
-    const { result } = loadSurveyByIdRepositorySpy
+    const { surveyModel } = loadSurveyByIdRepositorySpy
     expect(surveyResult).toEqual({
-      surveyId: result.id,
-      question: result.question,
-      date: result.date,
-      answers: result.answers.map(answer => ({
+      surveyId: surveyModel.id,
+      question: surveyModel.question,
+      date: surveyModel.date,
+      answers: surveyModel.answers.map(answer => ({
         ...answer,
         count: 0,
         percent: 0,
@@ -77,10 +68,9 @@ describe('DbLoadSurveyResult UseCase', () => {
       }))
     })
   })
-
   test('Should return surveyResultModel on success', async () => {
     const { sut, loadSurveyResultRepositorySpy } = makeSut()
     const surveyResult = await sut.load(surveyId, accountId)
-    expect(surveyResult).toEqual(loadSurveyResultRepositorySpy.result)
+    expect(surveyResult).toEqual(loadSurveyResultRepositorySpy.surveyResultModel)
   })
 })
